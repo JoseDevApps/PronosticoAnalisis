686b2aa7ff0f7fc5e1d48df71683e17b4a546fd1
JoseDevApps <Lionhard_2.6@hotmail.com>
2021-08-26 20:55:26 -0400

    Limpieza del script e inclusion de salida de archivos en formato CSV\

--- compMeteo.py
+++ compMeteo.py
@@ -11,9 +11,11 @@ fileObs = 'MesObservaciones.csv' # nombre del archivo csv observado
 fileWRF = 'WRF_data/2021-07-22.csv' # nombre del archivo csv WRF extraido previamente
 datetimetagObs = 'TIME_STAMP' # nombre de la etiqueta para la columna datetime
 datetimetagWRF = 'Time' # nomnbre de la etiqueta para la columna datetime
-granularidad = 5 # en minutos de tipo entero
+granularidad = 5 # en minutos de tipo entero de salidas WRF
+granularidadFinal = 5 # en minutos de tipo entero para el dataframe resultante
 start_date = '2021-07-23' # Inicio de datos para analisis
 end_date = '2021-07-24' # fin de datos para analisis
+nombreOut = 'UyuniMesPrueba' # nombre del archivo de salida despues de juntarlos en un mismo dataframe
 # Lectura observados
 df = pd.read_csv(pathObs+'/'+fileObs)
 df[datetimetagObs] = pd.to_datetime(df[datetimetagObs])
@@ -38,6 +40,5 @@ df = df.loc[mask]
 mask = (dfp.index > start_date) & (dfp.index<= end_date)
 dfp = dfp.loc[mask]
 df_Total = df.merge(dfp, left_index=True, right_index=True)
-df_Total = df_Total.resample(str(granularidad)+'min').first()
-print(df_Total)
-print(df_Total.info())
+df_Total = df_Total.resample(str(granularidadFinal)+'min').first()
+df_Total.to_csv(nombreOut+'.csv')
